1788a21a8fc30e4ad2bf574308702fce28e83deb
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2018-11-26 14:28:07 +0900

    app移動に関するnuxt.config編集

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ const pkg = require('./package')
 
 module.exports = {
   mode: 'universal',
+  srcDir: 'app',
 
   /*
   ** Headers of the page
@@ -59,7 +60,7 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
-      
+
     }
   }
 }

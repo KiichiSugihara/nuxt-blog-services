e6db86e7414760131685478bf5585bf9ebc0930a
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2018-11-27 14:26:20 +0900

    アカウント登録とログイン処理の実装

---
+++ app/store/index.js
@@ -0,0 +1,39 @@
+
+// データ格納場所を定義
+export const state = () => ({
+  isLoggedIn: false,
+  user: null
+})
+
+// gettersでstateの初期データの参照
+// stateデータを加工したいときに表示
+export const getters = {
+  isLoggedIn:(state) => state.isLoggedIn,
+  user: (state) => state.user
+}
+
+// stateを更新する関数を登録
+export const mutations = {
+  setUser(state, { user }) {
+    state.user = user
+    state.isLoggedIn = true
+  }
+}
+
+//mutationで定義した関数を実行する関数登録
+// action 内では非同期処理を行う
+export const actions = {
+  async login({ commit }, { id }) {
+    const user = await this.$axios.$get(`/user/${id}.json`)
+    if (!user.id) throw new Error('Invaild user')
+    commit('setUser', { user })
+  },
+  async register({ commit }, { id }) {
+    const payload = {}
+    payload[id] = { payload }
+    await this.$axios.$patch(`/users.json`, payload)
+    const user = await this.$axios.$get(`/users/${id}.json`)
+    if (!user.id) throw new Error('Invalid user')
+    commit ('setUser',{user})
+  }
+}
